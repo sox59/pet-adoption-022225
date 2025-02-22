243f9c0ea0b2eba6e5d54e67ffe183df33094e00
Paul <ppagliughi@yahoo.com>
2025-02-22 16:37:50 -0600

    Finished Dynamic Temperature

---
+++ main.js
@@ -0,0 +1,10 @@
+async function start() {
+  const weatherPromise = await fetch("https://api.weather.gov/gridpoints/LOT/73,78/forecast")
+  const weatherData = await weatherPromise.json()
+
+  const ourTemperature = weatherData.properties.periods[0].temperature
+  document.querySelector("#temperature-output").textContent = ourTemperature
+
+}
+
+start()
